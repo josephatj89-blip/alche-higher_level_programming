63aa5d583389ed6c9a9b52a97e18d45e114129fd
Josephat <josephatj89@gmail.com>
2026-08-20 12:54:46 -0700

    one more

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that provides a function to compute a to the power of b."""
+
+
+def pow(a, b):
+    """Return the value of a raised to the power of b."""
+    return a ** b
