a68112e2af1098b3b1aae1d9543eb2e728800f72
Josephat <josephatj89@gmail.com>
2026-08-21 00:34:32 -0700

    testing

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Imports add from add_0 and prints the sum of a and b"""
 from add_0 import add
 
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
-
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
