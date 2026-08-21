c0dc548e765b8aab21f423f78d7a15c2faca5915
Josephat <josephatj89@gmail.com>
2026-08-21 01:55:57 -0700

    looking

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Task 2: imports functions from calculator_1 and prints results"""
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
