4d39cc30895e25347d60cc7c4f40190790af7799
Josephat <josephatj89@gmail.com>
2026-08-22 05:13:39 -0700

    replace

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines replace_in_list."""
+
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element in a list at a specific position, like in C."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
