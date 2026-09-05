a5043443ef1f5b99588ae07dd4df57d00018cdf3
Josephat <josephatj89@gmail.com>
2026-09-05 06:10:35 -0700

    raise a nameError

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for raise_exception_msg function."""
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with the given message."""
+    raise NameError(message)
