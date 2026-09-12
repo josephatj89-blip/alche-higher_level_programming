c268bc38fb95741d702301437eb90fa6227db6aa
Josephat <josephatj89@gmail.com>
2026-09-12 01:15:55 -0700

    inheriting from class list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that defines class myclass inherited from the class list"""
+
+class myclass(list)
+   """class inheriting from class list"""
+
+
+def print_sorted(self):
+    """print the list sorted in ascending order"""
+    print(sorted(self))
