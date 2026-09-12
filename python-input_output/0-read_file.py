7fe556caf1146244cc3cefe4ced7b23af81a80f5
Josephat <josephatj89@gmail.com>
2026-09-12 05:05:53 -0700

    testing

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that reads and prints the content of a UTF-8 text file."""
+
+
 def read_file(filename=""):
+    """Read a text file (UTF8) and print it to stdout."""
     with open(filename, encoding="UTF8") as f:
         print(f.read(), end="")
