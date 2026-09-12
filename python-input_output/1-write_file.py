0f39efc9352cd8cdfe5711158d57be353009b606
Josephat <josephatj89@gmail.com>
2026-09-12 05:21:15 -0700

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that writes a string to a UTF-8 text file."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file (UTF8) and return the number
+    of characters written.
+    """
+    with open(filename, "w", encoding="UTF8") as f:
+        return f.write(text)
