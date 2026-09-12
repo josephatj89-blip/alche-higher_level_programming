768bb3cdd8550b83859b9100e8bbc806b07c4c20
Josephat <josephatj89@gmail.com>
2026-09-12 01:04:07 -0700

    testing

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module that returns the list of available attributes and methods
+of an object.
+"""
+
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object."""
     return dir(obj)
