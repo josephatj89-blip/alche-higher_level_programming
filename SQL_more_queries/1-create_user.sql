97c6f006372b4798c8ab53d8f72c328bae58e949
Josephat <josephatj89@gmail.com>
2026-09-13 05:26:37 -0700

    checking

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- creates the MySQL server user user_0d_1 with all privileges (no failure if it already exists)
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates the user if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Gives the user all privileges on the MySQL server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
