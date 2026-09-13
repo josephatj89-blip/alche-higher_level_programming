6eefe2a9970c6bb5bbf76feb81d5dca36170582f
Josephat <josephatj89@gmail.com>
2026-09-13 05:52:09 -0700

    states table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table states (no failure if either already exists)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
