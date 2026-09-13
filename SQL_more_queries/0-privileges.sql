fd5efcda75ea0d0239d6e3f46386f7cc5f709fde
Josephat <josephatj89@gmail.com>
2026-09-13 05:12:06 -0700

    testing

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,3 @@
--- Create user_0d_1 with full (root-like) privileges
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2 with limited privileges on user_2_db
-CREATE USER 'user_0d_2'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Show privileges for both users
+-- lists all privileges of user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
