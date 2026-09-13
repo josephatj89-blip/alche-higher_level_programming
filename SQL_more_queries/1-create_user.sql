5f7dc16ab3d263ed59ca1c8d60480fd765dd5e38
Josephat <josephatj89@gmail.com>
2026-09-13 05:31:05 -0700

    checking

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,10 @@
--- Creates the user if it does not already exist
+-- Create user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
--- Gives the user all privileges on the MySQL server
+-- Make sure the password is correct even if the user already existed
+ALTER USER 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Give all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
